8339cc2c36fb7f53c72808fb5336baa3deabffcf
Chris Fordham <chris@xhost.com.au>
2013-11-20 17:11:50 +1100

    Add rightscale/password to default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['right_api_client']['configure_users'] = [ 'rightscale' ]
 default['right_api_client']['rightscale']['account_id'] = nil
 default['right_api_client']['rightscale']['user'] = nil
+default['right_api_client']['rightscale']['password'] = nil
 default['right_api_client']['rightscale']['api_version'] = '1.5'
 default['right_api_client']['rightscale']['api_url'] = 'htts://us-3.rightscale.com'
 default['right_api_client']['test'] = true
